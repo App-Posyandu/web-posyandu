83f4505a2167be2b8a2ae8e50dddb59d336f2405
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-07-02 15:27:19 +0700

    feat: tambahkan fitur pencarian berdasarkan nomor telepon di list pengguna

--- resources/views/livewire/user-index.blade.php
+++ resources/views/livewire/user-index.blade.php
@@ -133,7 +133,7 @@
                 
                 <div class="flex-1 relative">
                     <input type="text" wire:model.live.debounce.300ms="search"
-                        placeholder="Cari nama, email, NIK..."
+                        placeholder="Cari nama, email, NIK, No. Telepon..."
                         class="w-full pl-4 pr-10 py-2 border border-gray-300 rounded-md text-sm focus:ring-pink-500 focus:border-pink-500">
                     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                         <i class="bi bi-search text-gray-400"></i>
