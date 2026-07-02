95881334273b9a4846129bd997c675a24bfcf5d7
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-07-02 15:34:39 +0700

    style: ubah wrapper dashboard menjadi transparan di desktop dan tetap putih di mobile

--- resources/views/dashboard/partials/admin.blade.php
+++ resources/views/dashboard/partials/admin.blade.php
@@ -1,6 +1,6 @@
 @section('content')
     <div class="w-full mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden sm:shadow-xl sm:rounded-2xl p-4 sm:p-6 lg:p-8 min-h-screen sm:min-h-0">
+        <div class="bg-white lg:bg-transparent overflow-hidden sm:shadow-xl lg:shadow-none sm:rounded-2xl p-4 sm:p-6 lg:p-8 min-h-screen sm:min-h-0">
             <div x-data="dashboardFilter" x-init="init()"
                 class="flex flex-col gap-4 md:gap-6 w-full mx-auto">
         @if (!$isVerified)
