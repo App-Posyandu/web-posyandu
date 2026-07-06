12ada6b834f42dcdff4e76e9420c012d83e8d09c
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-07-06 17:41:27 +0700

    style: tambahkan wrapper overflow-x-auto agar tabel takeover pengajuan responsif di mobile

--- resources/views/admin/users/takeover.blade.php
+++ resources/views/admin/users/takeover.blade.php
@@ -35,7 +35,7 @@
                         <span class="text-sm text-gray-500">Total: {{ $kaders->count() }} Kader</span>
                     </div>
 
-                    <div class="overflow-x-auto">
+                    <div class="w-full overflow-x-auto">
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
